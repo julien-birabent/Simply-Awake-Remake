4cd543ce717f13537c236c7d57f2f6e7d681cfe9
Julien Birabent <jul.birabent@gmail.com>
2024-08-30 14:12:10 -0400

    Add the notification manager to show the control notification for playback

---
+++ app/src/main/java/com/example/simplyawakeremake/notifications/SimplyAwakeNotificationManager.kt
@@ -0,0 +1,151 @@
+package com.example.simplyawakeremake.notifications
+
+import android.app.PendingIntent
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import androidx.media3.common.Player
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import androidx.media3.ui.PlayerNotificationManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.example.simplyawakeremake.R
+import com.google.common.util.concurrent.ListenableFuture
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+/**
+ * The size of the large icon for the notification in pixels.
+ */
+const val NOTIFICATION_LARGE_ICON_SIZE = 144 // px
+
+/**
+ * The channel ID for the notification.
+ */
+const val NOW_PLAYING_CHANNEL_ID = "org.julienbirabent.android.simplyawakeremake"
+
+/**
+ * The notification ID.
+ */
+const val NOW_PLAYING_NOTIFICATION_ID = 0xb339 // Arbitrary number used to identify our notification
+
+/**
+ * Default options for Glide.
+ */
+private val glideOptions = RequestOptions()
+    .fallback(R.drawable.ic_notification)
+    .diskCacheStrategy(DiskCacheStrategy.DATA)
+
+
+/**
+ * A wrapper class for ExoPlayer's PlayerNotificationManager.
+ * It sets up the notification shown to the user during audio playback and provides track metadata,
+ * such as track title and icon image.
+ * @param context The context used to create the notification.
+ * @param sessionToken The session token used to build MediaController.
+ * @param player The ExoPlayer instance.
+ * @param notificationListener The listener for notification events.
+ */
+@androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
+class SimplyAwakeNotificationManager(
+    private val context: Context,
+    sessionToken: SessionToken,
+    private val player: Player,
+    notificationListener: PlayerNotificationManager.NotificationListener
+) {
+    private val serviceJob = SupervisorJob()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+    private val notificationManager: PlayerNotificationManager
+
+    init {
+
+        val mediaController = MediaController.Builder(context, sessionToken).buildAsync()
+
+        notificationManager = PlayerNotificationManager.Builder(
+            context,
+            NOW_PLAYING_NOTIFICATION_ID,
+            NOW_PLAYING_CHANNEL_ID
+        )
+            .setChannelNameResourceId(R.string.notification_channel)
+            .setChannelDescriptionResourceId(R.string.notification_channel_description)
+            .setMediaDescriptionAdapter(DescriptionAdapter(mediaController))
+            .setNotificationListener(notificationListener)
+            .setSmallIconResourceId(R.drawable.ic_notification)
+            .build()
+            .apply {
+                setPlayer(player)
+                setUseRewindAction(true)
+                setUseRewindActionInCompactView(true)
+
+                setUseFastForwardAction(false)
+                setUseFastForwardActionInCompactView(false)
+            }
+
+    }
+
+    /**
+     * Hides the notification.
+     */
+    fun hideNotification() {
+        notificationManager.setPlayer(null)
+    }
+
+    /**
+     * Shows the notification for the given player.
+     * @param player The player instance for which the notification is shown.
+     */
+    fun showNotificationForPlayer(player: Player) {
+        notificationManager.setPlayer(player)
+    }
+
+    private inner class DescriptionAdapter(private val controller: ListenableFuture<MediaController>) :
+        PlayerNotificationManager.MediaDescriptionAdapter {
+
+        var currentIconUri: Uri? = null
+        var currentBitmap: Bitmap? = null
+
+        override fun createCurrentContentIntent(player: Player): PendingIntent? =
+            controller.get().sessionActivity
+
+        override fun getCurrentContentText(player: Player) = ""
+
+        override fun getCurrentContentTitle(player: Player) = controller.get().mediaMetadata.title.toString()
+
+        override fun getCurrentLargeIcon(
+            player: Player,
+            callback: PlayerNotificationManager.BitmapCallback
+        ): Bitmap? {
+            val iconUri = controller.get().mediaMetadata.artworkUri
+            return if (currentIconUri != iconUri || currentBitmap == null) {
+
+                // Cache the bitmap for the current song so that successive calls to
+                // `getCurrentLargeIcon` don't cause the bitmap to be recreated.
+                currentIconUri = iconUri
+                serviceScope.launch {
+                    currentBitmap = iconUri?.let {
+                        resolveUriAsBitmap(it)
+                    }
+                    currentBitmap?.let { callback.onBitmap(it) }
+                }
+                null
+            } else {
+                currentBitmap
+            }
+        }
+
+        private suspend fun resolveUriAsBitmap(uri: Uri): Bitmap? {
+            return withContext(Dispatchers.IO) {
+                Glide.with(context).applyDefaultRequestOptions(glideOptions)
+                    .asBitmap()
+                    .load(uri)
+                    .submit(NOTIFICATION_LARGE_ICON_SIZE, NOTIFICATION_LARGE_ICON_SIZE)
+                    .get()
+            }
+        }
+    }
+}

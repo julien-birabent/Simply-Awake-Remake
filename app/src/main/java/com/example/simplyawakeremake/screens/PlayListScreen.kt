f3fd16844517722dd6a852497eae0d8d475c669d
Julien Birabent <jul.birabent@gmail.com>
2024-09-17 15:49:36 -0400

    Update the style of the meditation list

--- app/src/main/java/com/example/simplyawakeremake/screens/PlayListScreen.kt
+++ app/src/main/java/com/example/simplyawakeremake/screens/PlayListScreen.kt
@@ -3,16 +3,18 @@ package com.example.simplyawakeremake.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,10 +23,12 @@ import androidx.compose.runtime.rxjava3.subscribeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.simplyawakeremake.R
 import com.example.simplyawakeremake.UiTrack
 import com.example.simplyawakeremake.navigation.Screen
 import com.example.simplyawakeremake.viewmodel.PlayerListUIState
@@ -46,7 +50,19 @@ fun PlayListScreen(navController: NavController, viewModel: TrackListViewModel =
         }
 
         is PlayerListUIState.Tracks -> {
-            Playlist(tracks = (uiState as PlayerListUIState.Tracks).items, navController)
+            Column(modifier = Modifier.padding(vertical = 12.dp)) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+                Spacer(modifier = Modifier.size(12.dp))
+                Playlist(tracks = (uiState as PlayerListUIState.Tracks).items, navController)
+            }
         }
     }
 }
@@ -55,14 +71,14 @@ fun PlayListScreen(navController: NavController, viewModel: TrackListViewModel =
 fun Playlist(tracks: List<UiTrack>, navController: NavController) {
     LazyColumn(
         modifier = Modifier.fillMaxWidth(),
-        contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(
             count = tracks.size,
             key = { tracks[it].id },
             itemContent = { index ->
                 TrackItem(tracks[index]) { id -> navController.navigate(Screen.NOW_PLAYING.name + "/${id}") }
+                if (index < tracks.lastIndex)
+                    HorizontalDivider(color = Color.White, thickness = 1.dp)
             }
         )
     }
@@ -70,62 +86,44 @@ fun Playlist(tracks: List<UiTrack>, navController: NavController) {
 
 @Composable
 fun TrackItem(track: UiTrack, navigateToTrack: (id: String) -> Unit) {
-    Card(
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surfaceVariant,
-        )
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.Center)
+            .clickable(onClick = { navigateToTrack(track.id) })
+            .padding(12.dp)
     ) {
-        Column(
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .wrapContentSize(Alignment.Center)
-                .clickable(onClick = { navigateToTrack(track.id) })
-                .background(Color.White)
-                .padding(top = 8.dp, bottom = 8.dp)
+                .wrapContentHeight(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                modifier = Modifier
-                    .padding(horizontal = 8.dp)
-                    .fillMaxWidth()
-                    .wrapContentHeight(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    "${track.ordinal} " + track.displayName,
-                    style = MaterialTheme.typography.bodyLarge,
-                    textAlign = TextAlign.Left,
-                    modifier = Modifier.fillMaxWidth()
-                )
-            }
-            Row(
-                modifier = Modifier
-                    .padding(horizontal = 8.dp)
-                    .fillMaxWidth()
-                    .wrapContentHeight(),
-                horizontalArrangement = Arrangement.Start,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = track.tagString,
-                    textAlign = TextAlign.Left,
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .padding(horizontal = 8.dp),
-                horizontalArrangement = Arrangement.Start,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "Duration ${track.duration}",
-                    textAlign = TextAlign.Left,
-                    style = MaterialTheme.typography.bodySmall
-                )
-            }
+            Text(
+                "${track.ordinal} " + track.displayName,
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Start
+            )
+            Text(
+                text = "${track.duration}",
+                textAlign = TextAlign.Right,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = track.tagString,
+                textAlign = TextAlign.Left,
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 }

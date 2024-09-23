9ec1119a8a9e12fca94de338c30dfb26c9ad6302
Julien Birabent <jul.birabent@gmail.com>
2024-09-23 12:27:12 -0400

    Remove previously added POST NOTIFICATION Permission - unecessary

--- app/src/main/java/com/example/simplyawakeremake/screens/MainActivity.kt
+++ app/src/main/java/com/example/simplyawakeremake/screens/MainActivity.kt
@@ -1,21 +1,17 @@
 package com.example.simplyawakeremake.screens
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
 import com.example.simplyawakeremake.navigation.AppNavHost
-import com.example.simplyawakeremake.notifications.RequestNotificationPermissions
 import com.example.simplyawakeremake.ui.theme.SimplyAwakeRemakeTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -29,7 +25,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) RequestNotificationPermissions()
                     AppNavHost(navController = rememberNavController())
                 }
             }

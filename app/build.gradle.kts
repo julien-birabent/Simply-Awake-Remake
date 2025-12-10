fbfc02fc796950b7ebb08e289d72ea4b25836b23
Julien Birabent <jul.birabent@gmail.com>
2025-12-10 21:38:52 +0100

    Chore: Bump version code to 13
    This commit increments the `versionCode` from 12 to 13 in the `app/build.gradle.kts` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "org.marklackey.android.awake"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
+        versionCode = 13
         versionName = "11.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

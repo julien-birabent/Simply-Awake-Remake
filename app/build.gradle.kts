0ca87a045505d0c4a6fd77ab13e7080d33920e1f
Julien Birabent <jul.birabent@gmail.com>
2025-12-26 16:09:56 +0100

    Build: Increment version to 11.1
    This commit updates the application versioning in the Gradle configuration.
    
    - Updated `versionCode` from 13 to 14.
    - Updated `versionName` from 11.0 to 11.1.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "org.marklackey.android.awake"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-        versionName = "11.0"
+        versionCode = 14
+        versionName = "11.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
